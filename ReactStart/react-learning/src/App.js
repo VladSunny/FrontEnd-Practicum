1c8404d8087ef68c2451fb5ad80a3db18cf25c71
Vlad Sunny <vladislavsunny@gmail.com>
2024-07-15 15:57:01 +0300

    tailwind test

--- ReactStart/react-learning/src/App.js
+++ ReactStart/react-learning/src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-      <img src={logo} className="App-logo" alt="logo" />
+    <div class="flex flex-row justify-around">
+      <button class="size-11 bg-red-500">Test</button>
+      <img width={60} height={60} src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
